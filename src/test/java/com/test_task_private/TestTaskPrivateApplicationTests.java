51d340b01b9e90f13de6c3e25ab9dcff41f2837c
Mykola Horianin <anyavic2379@gmail.com>
2024-07-23 13:59:40 +0300

    first commit

---
+++ src/test/java/com/test_task_private/TestTaskPrivateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test_task_private;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskPrivateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
